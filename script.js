09fa262732359ad786eb208e1a4633f0424822bf
Pial Uddin <146668884+IamPial@users.noreply.github.com>
2025-05-02 18:25:07 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+/**
+ * Date: 01.05.2025
+ * Author : Pial Uddin
+ * Description : Let's create a counter application with some fancy requirements
+ */
+
+//finding all the necessary elements
+const resultBody = document.getElementById('result-body');
+const incrementInput = document.getElementById('increment-input');
+const decrementInput = document.getElementById('decrement-input');
+const buttonIncrement = document.getElementById('increment-btn');
+const buttonDecrement = document.getElementById('decrement-btn');
+
+
+
+let counter;
+
+
+buttonIncrement.addEventListener('click', function(){
+    let increment = parseInt(incrementInput.value);
+    let result = parseInt(resultBody.innerHTML);
+
+    counter = increment + result;
+    
+    if(counter <= 9){
+        resultBody.innerHTML = `0${counter}`;
+    }
+    else{
+        resultBody.innerHTML = counter;
+    }
+    incrementInput.value = '1';
+
+});
+
+
+
+buttonDecrement.addEventListener('click', function(){
+
+    let decrement = parseInt(decrementInput.value);
+    let result = parseInt(resultBody.innerHTML);
+  
+    counter = result - decrement;
+
+    if(counter < 0){
+        alert('Negative values are not allowed!');
+    }
+    else{
+        if(counter <= 9){
+            resultBody.innerHTML = `0${counter}`;
+        }
+        else{
+            resultBody.innerHTML = counter;
+            
+        }
+    }
+    decrementInput.value = "1";
+})
+
+
+
+
+
+
+
